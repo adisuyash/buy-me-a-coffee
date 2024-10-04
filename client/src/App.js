92bf3e29b56ba22eacdf90815f5617ad1e76c112
adisuyash <adityasuyashgupta@gmail.com>
2024-10-05 04:31:16 +0530

    Add robots.txt, App.test.js, .gitignore, index.css, reportWebVitals.js, manifest.json, index.js, App.css, package.json, and index.html files

---
+++ client/src/App.js
@@ -0,0 +1,74 @@
+import abi from "./contract/chai.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import Buy from "./components/Buy";
+import Memos from "./components/Memos";
+import chai from "./chai.png";
+import "./App.css";
+
+function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+  const [account, setAccount] = useState("None");
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0x46436dcb1b29b111a00bb61f5475b420ef1104eb";
+      const contractABI = abi.abi;
+      try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+          const account = await ethereum.request({
+            method: "eth_requestAccounts",
+          });
+
+          window.ethereum.on("chainChanged", () => {
+            window.location.reload();
+          });
+
+          window.ethereum.on("accountsChanged", () => {
+            window.location.reload();
+          });
+
+          // Updated ethers v6 syntax
+          const provider = new ethers.BrowserProvider(ethereum);
+          const signer = await provider.getSigner();
+          const contract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            signer
+          );
+          setAccount(account);
+          setState({ provider, signer, contract });
+        } else {
+          alert("Please install metamask");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    connectWallet();
+  }, []);
+
+  return (
+    <div style={{ backgroundColor: "#EFEFEF", height: "100px" }}>
+      <img src={chai} className="img-fluid" alt="chai" width="100px" />
+      <p
+        className="text-muted lead"
+        style={{ marginTop: "10px", marginLeft: "5px" }}
+      >
+        <small>Connected Account - {account}</small>
+      </p>
+      <div className="container">
+        <Buy state={state} />
+        <Memos state={state} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
