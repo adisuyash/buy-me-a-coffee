c0c5cd464953bfe7c83b03208dcdff0646184c8f
Mateus Nunes <mateusnss@protonmail.ch>
2024-10-06 13:13:08 -0300

    🍬 added native application colors to tailwind configuration file

--- client/src/tailwind.config.js
+++ client/src/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
       "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+            'default-sky-blue': '#61dafb',
+            'default-lemon-yellow': '#f4f666'
+        }
+      },
     },
     plugins: [],
   };
